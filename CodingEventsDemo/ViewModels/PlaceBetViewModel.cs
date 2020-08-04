3d4090489c26970d651691c8544aa404b011d07f
Shawn C <scooter4356@gmail.com>
2020-08-03 22:10:09 -0500

    view models added. logs in and out

---
+++ CodingEventsDemo/ViewModels/PlaceBetViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Roulette_Identity.ViewModels
+{
+    public class PlaceBetViewModel
+    {
+        public string Type { get; set; }
+        public int Amount { get; set; }
+    }
+}
